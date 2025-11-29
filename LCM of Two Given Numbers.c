b8bc137a992ec479b6290344dd25eb50a717a115
Pranamya-Pedhamalla <pedhamalla@gmail.com>
2025-11-29 14:37:34 +0530

    LCM of Two Given Numbers.c

---
+++ LCM of Two Given Numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,max,s;
+scanf("%d%d",&a,&b);
+S = 0;
+max = (a>b)?a:b;
+while(1){
+if(max%a == 0&&max%b == 0){
+
+}
+max++;
+}
+printf("%d",max);
+return 0;
+
+}
